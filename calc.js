06d70b3acc7d0e115d1bdc47b1733711f059391c
Swear <ryanmswearingen@gmail.com>
2020-10-20 22:53:32 -0700

    ops and digits 1

--- calc.js
+++ calc.js
@@ -1,13 +1,44 @@
-let calcs = {};
-let operands = {};
-
+let calcs = [];
+let operands = [];
+let noReset = false;
 
 
 function init(){
-    const screen = document.getElementById('text');
+    screen = document.getElementById('text');
     screen.innerText = '0.0';
+
+    digits = document.getElementsByClassName('digit');
+    for (let i = 0; i < digits.length; i++)
+    {
+        digits[i].addEventListener('click', function() {digitPressed(screen, digits[i].innerText)});
+        
+    }
+
+    ops = document.getElementsByClassName('op');
+    for (let i = 0; i < ops.length; i++)
+    {
+        ops[i].addEventListener('click', function() {opPressed(screen, ops[i].innerText)});
+        
+    }
+    //but7 = document.getElementById('but7');
+    //but7.addEventListener('click', digitPressed(screen));
 }
 
+function digitPressed(screen, number){
+    if (noReset){
+    screen.innerText = screen.innerText + number;
+    }else{
+        screen.innerText = number;
+        noReset = true;
+    }
+}
+
+function opPressed(screen, operand)
+{
+    screen.innerText = '';
+    operands.push(operand);
+    console.log(operands);
+}
 
 
 
